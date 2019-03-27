201fdd12934ad0cdc5edde139eff0bc3d4ecf824
Gabe LaPoint <glapoint22@gmail.com>
2019-03-27 16:06:36 -0400

    Updated filters

--- projects/website/src/app/pages/search/components/filter/filter.component.ts
+++ projects/website/src/app/pages/search/components/filter/filter.component.ts
@@ -1,18 +1,14 @@
-import { Component, Input, OnInit, HostListener } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss']
 })
-export class FilterComponent implements OnInit {
+export class FilterComponent {
   @Input() caption: string;
-  public showContent: boolean;
-  public show: boolean;
-
-  ngOnInit() {
-    window.dispatchEvent(new Event('resize'));
-  }
+  @Input() showContent: boolean;
+  @Input() show: boolean;
 
   onArrowClick() {
     if (!this.show) {
@@ -27,18 +23,5 @@ export class FilterComponent implements OnInit {
 
   onTransitionEnd() {
     if (!this.showContent) this.show = false;
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    window.setTimeout(() => {
-      if (event.target.innerWidth > 970) {
-        this.show = true;
-        this.showContent = true;
-      } else {
-        this.show = false;
-        this.showContent = false;
-      }
-    }, 1)
   }
 }
