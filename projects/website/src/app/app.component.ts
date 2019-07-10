63b31ea11ee147d9681b0c94dff2e70b9321fb62
Gabe LaPoint <glapoint22@gmail.com>
2019-07-10 19:01:47 -0400

    Started share

--- projects/website/src/app/app.component.ts
+++ projects/website/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { DataService } from 'src/app/services/data/data.service';
-import { Router, NavigationEnd, NavigationStart, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -19,5 +18,36 @@ export class AppComponent implements OnInit {
         response;
       });
     }
+
+
+    if (!window['fbAsyncInit']) {
+      window['fbAsyncInit'] = function () {
+          window['FB'].init({
+              appId: '167320040479385',
+              autoLogAppEvents: true,
+              xfbml: true,
+              version: 'v3.0'
+          });
+
+
+          // window['FB'].ui({
+          //   method: 'share',
+          //   href: 'https://developers.facebook.com/docs/',
+          // }, function (response) { });
+
+      };
+  }
+
+  // load facebook sdk if required
+  const url = 'https://connect.facebook.net/en_US/sdk.js';
+  if (!document.querySelector(`script[src='${url}']`)) {
+      let script = document.createElement('script');
+      script.src = url;
+      document.body.appendChild(script);
+  }
+
+
+
+
   }
 } 
