85aaa2df647a35e5c38c8dd3cc308926c36eb74e
Gabe LaPoint <glapoint22@gmail.com>
2019-07-29 14:53:32 -0400

    Finished orders

--- projects/website/src/app/pages/orders/orders.component.ts
+++ projects/website/src/app/pages/orders/orders.component.ts
@@ -3,7 +3,7 @@ import { Subject } from 'rxjs';
 import { Page } from '../page';
 import { Title, Meta } from '@angular/platform-browser';
 import { DOCUMENT } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'orders',
@@ -14,92 +14,24 @@ export class OrdersComponent extends Page implements OnInit {
   public event = new Subject<void>();
   public origin: number = 2019;
   public filter: Array<any>;
-
-  public orders = [
-    {
-      date: 'May 22, 2019',
-      orderNumber: 'CWOGBZLN',
-      mainProduct: {
-        id: 'FRT6YHJE4J',
-        hoplink: 'https://201behydk0sr8n2-f2jo9qcq9u.hop.clickbank.net/',
-        urlTitle: 'fat-loss-activation'
-      },
-      products: [
-        {
-          title: 'The 2 Week Diet Audiobook Companion (Listen On Any Device!)',
-          type: 'Physical product',
-          quantity: 2,
-          price: 4.95,
-          image: 'e9a794bc40f14f709e6636aefbfe5d43.png'
-        },
-        {
-          title: 'The 2 Week Diet Audiobook Companion (Listen On Any Device!)',
-          type: 'Physical product',
-          quantity: 2,
-          price: 4.95,
-          image: null
-        },
-        {
-          title: 'The 2 Week Diet Audiobook Companion (Listen On Any Device!)',
-          type: 'Physical product',
-          quantity: 2,
-          price: 4.95,
-          image: null
-        }
-      ],
-      paymentMethod: 'MAES',
-      subtotal: 65.04,
-      shippingHandling: 5,
-      discount: -2,
-      tax: 1.07,
-      total: 69.11
-    },
-    {
-      date: 'May 22, 2019',
-      orderNumber: 'CWOGBZLN',
-      products: [
-        {
-          title: 'The 2 Week Diet Audiobook Companion (Listen On Any Device!)',
-          type: 'Physical product',
-          quantity: 2,
-          price: 4.95,
-          image: 'e9a794bc40f14f709e6636aefbfe5d43.png'
-        },
-        {
-          title: 'The 2 Week Diet Audiobook Companion (Listen On Any Device!)',
-          type: 'Physical product',
-          quantity: 2,
-          price: 4.95,
-          image: null
-        },
-        {
-          title: 'The 2 Week Diet Audiobook Companion (Listen On Any Device!)',
-          type: 'Physical product',
-          quantity: 2,
-          price: 4.95,
-          image: null
-        }
-      ],
-      paymentMethod: 'MAES',
-      subtotal: 65.04,
-      shippingHandling: 5,
-      discount: -2,
-      tax: 1.07,
-      total: 69.11
-    }
-  ]
-
+  public orders: Array<any>;
   public selectedFilter;
+  public search: string;
+  public searchResults: Array<any>;
+  public displayType: string;
 
   constructor(
     titleService: Title,
     metaService: Meta,
     @Inject(DOCUMENT) document,
-    private router: Router
-  ) {super(titleService, metaService, document) }
+    private router: Router,
+    private route: ActivatedRoute
+  ) { super(titleService, metaService, document) }
+
+
 
   ngOnInit() {
-    this.title = 'Your Orders';
+    // Set the filter options
     this.filter = [
       {
         name: 'Last 30 days',
@@ -111,6 +43,7 @@ export class OrdersComponent extends Page implements OnInit {
       }
     ]
 
+    // Add the years to the filter options
     let currentYear = new Date().getFullYear();
     do {
       this.filter.push({
@@ -121,18 +54,155 @@ export class OrdersComponent extends Page implements OnInit {
     }
     while (currentYear >= this.origin)
 
-
+    // Select the default filter
     this.selectedFilter = this.filter[1];
+
+    // Set the page properties
+    this.title = 'Your Orders';
     this.share = false;
     super.ngOnInit();
+
+    this.route.queryParamMap.subscribe((queryParams: ParamMap) => {
+      this.search = queryParams.get('search');
+
+      // Orders
+      this.orders = [
+        {
+          date: 'May 22, 2019',
+          orderNumber: 'CWOGBZLN',
+          mainProduct: {
+            id: 'FRT6YHJE4J',
+            hoplink: 'https://201behydk0sr8n2-f2jo9qcq9u.hop.clickbank.net/',
+            urlTitle: 'fat-loss-activation'
+          },
+          products: [
+            {
+              title: 'The 2 Week Diet Audiobook Companion (Listen On Any Device!)',
+              type: 'Physical product',
+              quantity: 2,
+              price: 4.95,
+              image: 'e9a794bc40f14f709e6636aefbfe5d43.png'
+            },
+            {
+              title: 'The 2 Week Diet Audiobook Companion (Listen On Any Device!)',
+              type: 'Physical product',
+              quantity: 2,
+              price: 4.95,
+              image: null
+            },
+            {
+              title: 'The 2 Week Diet Audiobook Companion (Listen On Any Device!)',
+              type: 'Physical product',
+              quantity: 2,
+              price: 4.95,
+              image: null
+            }
+          ],
+          paymentMethod: 'MAES',
+          subtotal: 65.04,
+          shippingHandling: 5,
+          discount: -2,
+          tax: 1.07,
+          total: 69.11
+        },
+        {
+          date: 'May 22, 2019',
+          orderNumber: 'CWOGBZLN',
+          products: [
+            {
+              title: 'The 2 Week Diet Audiobook Companion (Listen On Any Device!)',
+              type: 'Physical product',
+              quantity: 2,
+              price: 4.95,
+              image: 'e9a794bc40f14f709e6636aefbfe5d43.png'
+            },
+            {
+              title: 'The 2 Week Diet Audiobook Companion (Listen On Any Device!)',
+              type: 'Physical product',
+              quantity: 2,
+              price: 4.95,
+              image: null
+            },
+            {
+              title: 'The 2 Week Diet Audiobook Companion (Listen On Any Device!)',
+              type: 'Physical product',
+              quantity: 2,
+              price: 4.95,
+              image: null
+            }
+          ],
+          paymentMethod: 'MAES',
+          subtotal: 65.04,
+          shippingHandling: 5,
+          discount: -2,
+          tax: 1.07,
+          total: 69.11
+        }
+      ]
+
+      // Search results
+      this.searchResults = [
+        {
+          date: 'May 22, 2019',
+          title: 'The 2 Week Diet Audiobook Companion (Listen On Any Device!)',
+          image: 'e9a794bc40f14f709e6636aefbfe5d43.png',
+          hoplink: 'https://201behydk0sr8n2-f2jo9qcq9u.hop.clickbank.net/',
+          orderNumber: 'CWOGBZLN',
+          numItems: 4
+        },
+        {
+          date: 'May 22, 2019',
+          title: 'The 2 Week Diet Audiobook Companion (Listen On Any Device!)',
+          image: 'e9a794bc40f14f709e6636aefbfe5d43.png',
+          hoplink: 'https://201behydk0sr8n2-f2jo9qcq9u.hop.clickbank.net/',
+          orderNumber: 'CWOGBZLN',
+          numItems: 4
+        },{
+          date: 'May 22, 2019',
+          title: 'The 2 Week Diet Audiobook Companion (Listen On Any Device!)',
+          image: 'e9a794bc40f14f709e6636aefbfe5d43.png',
+          hoplink: 'https://201behydk0sr8n2-f2jo9qcq9u.hop.clickbank.net/',
+          orderNumber: 'CWOGBZLN',
+          numItems: 4
+        },{
+          date: 'May 22, 2019',
+          title: 'The 2 Week Diet Audiobook Companion (Listen On Any Device!)',
+          image: 'e9a794bc40f14f709e6636aefbfe5d43.png',
+          hoplink: 'https://201behydk0sr8n2-f2jo9qcq9u.hop.clickbank.net/',
+          orderNumber: 'CWOGBZLN',
+          numItems: 4
+        }
+      ];
+
+      // This will be determined by the server - "product" or "order"
+      this.displayType = 'order'; 
+
+    });
   }
 
   onFilterClick(item) {
     this.selectedFilter = item;
     this.router.navigate([location.pathname], {
-      queryParams: { 'orderFilter': this.selectedFilter.value },
-      queryParamsHandling: 'merge'
+      queryParams: { 'orderFilter': this.selectedFilter.value }
     });
+  }
+
+  onSearch(search: string){
+    this.router.navigate([location.pathname], {
+      queryParams: { 'search': search }
+    });
+  }
+  
+  resetUrl(){
+    this.router.navigate([location.pathname], {
+      queryParams: { }
+    });
+  }
+
+  onSearchKeydown(event, search: string) {
+    if (event.code === 'NumpadEnter' || event.code === 'Enter' || event.keyCode === 13) {
+      this.onSearch(search);
+    }
   }
 
   @HostListener('document:mousedown', ['$event'])
@@ -169,11 +239,11 @@ export class OrdersComponent extends Page implements OnInit {
     if (method == 'MAES') return 'Maestro';
   }
 
-  getDefaultIndex(){
+  getDefaultIndex() {
     return this.filter.findIndex(x => x == this.selectedFilter);
   }
 
-  onWriteReviewClick(productId: string){
+  onWriteReviewClick(productId: string) {
     this.router.navigate(['/reviews/write-review'], { queryParams: { 'id': productId } });
   }
 
