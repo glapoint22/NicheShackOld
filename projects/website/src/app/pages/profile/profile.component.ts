c2b3ef23a2e009a995c254630a3e7d271142921a
Gabe LaPoint <glapoint22@gmail.com>
2019-07-31 19:43:15 -0400

    Working on account

--- projects/website/src/app/pages/profile/profile.component.ts
+++ projects/website/src/app/pages/profile/profile.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Page } from '../page';
+import { Title, Meta } from '@angular/platform-browser';
+import { DOCUMENT } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'profile',
@@ -7,11 +10,28 @@ import { Page } from '../page';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent extends Page implements OnInit {
+  public profile: any;
+
+  constructor(
+    titleService: Title,
+    metaService: Meta,
+    @Inject(DOCUMENT) document,
+    private router: Router) { super(titleService, metaService, document) }
 
   ngOnInit() {
     this.title = 'Profile';
     this.share = false;
     super.ngOnInit();
+
+    this.profile = {
+      firstName: 'Gabe',
+      lastName: 'LaPoint',
+      email: 'glapoint22@gmail.com',
+      password: 'Cyb668622'
+    }
   }
 
+  navigate(path: string){
+    this.router.navigate([path]);
+  }
 }
